a8a3545b8680de4874741479129080f8bce2d76b
ting <ttzhenng@gmail.com>
2021-01-07 22:36:03 -0500

    add readme

---
+++ lib/college_catalog.rb
@@ -0,0 +1,6 @@
+require "college_catalog/version"
+
+module CollegeCatalog
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/college_catalog/version.rb
@@ -0,0 +1,3 @@
+module CollegeCatalog
+  VERSION = "0.1.0"
+end
